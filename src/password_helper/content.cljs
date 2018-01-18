60ff28e9f4441d37c9786f93e9613d3e0cca6d71
Adam Pohorecki <adam@pohorecki.pl>
2018-01-18 22:11:31 +0100

    Add change handler to input

--- src/password_helper/content.cljs
+++ src/password_helper/content.cljs
@@ -1,6 +1,13 @@
 (ns password-helper.content
   (:require [khroma.log :as console]
+            [dommy.core :as dommy :refer-macros [sel1]]
             [hipo.core :as hipo]))
+
+(def debugging true)
+(defn- debug
+  "Prints a debug message if debugging is enabled"
+  [message]
+  (if debugging (console/log message)))
 
 (defn password-helper-box
   "The main HTML element injected into the page"
@@ -11,10 +18,24 @@
 (defn inject-html
   "Injects password helper HTML element into the current page"
   []
-  (console/log "Injecting Password Helper HTML")
+  (debug "Injecting Password Helper HTML")
   (.appendChild js/document.body
                 (hipo/create (password-helper-box))))
 
+(defn on-input-change
+  "This function is called whenever the password input changes with the new password value"
+  [password]
+  (debug (str "New password is \"" password "\"")))
+
+(defn listen-for-input-changes
+  "Registers event handler to listen to input changes on the password input"
+  []
+  (let [input (sel1 :#password-helper-input)
+        handler (fn [event] (on-input-change (dommy/value input)))]
+    (debug "Registering change handler")
+    (dommy/listen! input :keyup handler)))
+
 (defn init []
-  (console/log "Password Helper initialised!")
-  (inject-html))
+  (debug "Password Helper initialised!")
+  (inject-html)
+  (listen-for-input-changes))
