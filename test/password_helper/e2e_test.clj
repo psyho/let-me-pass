8aef9eecd5d9c2e52b7a474cbd2f2f2e24af02c2
Adam Pohorecki <adam@pohorecki.pl>
2018-01-27 21:40:09 +0100

    Add test for new alior page

--- test/password_helper/e2e_test.clj
+++ test/password_helper/e2e_test.clj
@@ -22,12 +22,20 @@
   :each ;; start and stop driver for each test
   fixture-driver)
 
-(defn idx-from-input-id
-  "Returns the index corresponding to the password input"
-  [input]
-  (let [id (get-element-attr-el *driver* input :id)
+(defn idx-from-input-based-on-attr [input attr]
+  (let [id (get-element-attr-el *driver* input attr)
         number (re-find #"\d+" id)]
     (dec (Integer/parseInt number))))
+
+(defn idx-from-input-id
+  "Returns the index corresponding to the password input, based on ID attribute"
+  [input]
+  (idx-from-input-based-on-attr input :id))
+
+(defn idx-from-input-name
+  "Returns the index corresponding to the password input, based on name attribute"
+  [input]
+  (idx-from-input-based-on-attr input :name))
 
 (defn verify-typing-input-via-helper [{:keys [login-url
                                               login-selector
@@ -35,10 +43,12 @@
                                               before-submit-login
                                               submit-login-selector
                                               before-fill-password
-                                              idx-from-input]
+                                              idx-from-input
+                                              after-fill-password]
                                        :or {valid-login "123123123"
                                             before-submit-login #()
-                                            before-fill-password #()}}]
+                                            before-fill-password #()
+                                            after-fill-password #()}}]
   (go *driver* login-url)
   (wait-visible *driver* login-selector)
   (fill *driver* login-selector valid-login)
@@ -47,12 +57,15 @@
   (before-fill-password)
   (wait-visible *driver* {:id :password-helper-input})
   (fill *driver* {:id :password-helper-input} password)
-  (let [password-inputs (query-all *driver* {:tag :input :type :password :maxlength 1})
-        password-inputs (remove #(get-element-attr-el *driver* % :readonly) password-inputs)]
+  (let [password-inputs
+        (->> (query-all *driver* {:tag :input :type :password :maxlength 1})
+             (remove #(get-element-attr-el *driver* % :readonly))
+             (remove #(get-element-attr-el *driver* % :disabled)))]
     (doseq [input password-inputs]
       (let [idx (idx-from-input input)
             char (str (nth password idx))]
-        (is (= char (get-element-value-el *driver* input)))))))
+        (is (= char (get-element-value-el *driver* input))))))
+  (after-fill-password))
 
 (deftest ing
   (verify-typing-input-via-helper {:login-url "https://login.ingbank.pl/"
@@ -71,3 +84,14 @@
                                                            (wait-visible *driver* {:tag :frameset})
                                                            (switch-frame *driver* {:tag :frame :name :main}))
                                    :idx-from-input idx-from-input-id}))
+
+(deftest alior-new
+  (verify-typing-input-via-helper {:login-url "https://system.aliorbank.pl/sign-in"
+                                   :login-selector {:id :login}
+                                   :valid-login "45573286"
+                                   :submit-login-selector {:tag :button :title "Next" :type :submit}
+                                   :idx-from-input idx-from-input-name
+                                   :after-fill-password (fn []
+                                                          (let [password-submit-btn (query *driver* {:id :password-submit})
+                                                                disabled (get-element-attr-el *driver* password-submit-btn :disabled)]
+                                                            (is (nil? disabled))))}))
