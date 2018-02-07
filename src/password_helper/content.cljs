d615fbd2e3d2286c4e3b45e3c2758daa52b64a76
Adam Pohorecki <adam@pohorecki.pl>
2018-02-07 20:39:47 +0100

    Make pick chars update the password inputs

--- src/password_helper/content.cljs
+++ src/password_helper/content.cljs
@@ -7,13 +7,16 @@
             [dommy.utils :refer [->Array]]
             [hipo.core :as hipo]))
 
+
 (def debugging true)
 (defn- debug
   "Prints a debug message if debugging is enabled"
   [message]
   (if debugging (console/log message)))
 
+
 (def running-inline (undefined? js/chrome.extension))
+
 
 (defn get-asset-url
   "Returns asset URL"
@@ -22,15 +25,75 @@
     (str "../resources/" type  "/" name)
     (extension/get-url name)))
 
+
 (defn get-css-asset-url
   "Returns CSS asset URL"
   [name]
   (get-asset-url "css" name))
 
+
 (defn get-js-asset-url
   "Returns JS asset URL"
   [name]
   (get-asset-url "js" name))
+
+
+(defn ignore-errors
+  "Executes the given function and ignores errors thrown (logs them to console)"
+  [f]
+  (try
+    (f)
+    (catch :default e
+      (console/warn e)
+      nil)))
+
+
+(defn frame-documents
+  "Returns document objects for all frames found in document"
+  [document]
+  (letfn [(content-documents [tag]
+            (->> (->Array (sel document tag))
+                 (map (fn [frame] (ignore-errors #(.-contentDocument frame))))
+                 (remove nil?)))]
+    (concat
+      (content-documents :frame)
+      (content-documents :iframe))))
+
+
+(defn all-frame-docs
+  "Returns a collection of all frame documents on the page (recursively) including the top-level document"
+  ([]
+   (all-frame-docs [js/document]))
+
+  ([documents]
+   (if (seq documents)
+     (let [first-doc (first documents)]
+       (lazy-seq (cons first-doc (all-frame-docs (concat (rest documents) (frame-documents first-doc))))))
+     (lazy-seq))))
+
+
+(defn partial-password-input?
+  "Checks if element looks like a partial password input"
+  [element]
+  (and
+    (= "password" (dommy/attr element :type))
+    (= "1" (dommy/attr element :maxlength))))
+
+
+(defn find-partial-password-inputs
+  "Scans the page for inputs that are characteristic to partial passwords"
+  [root]
+  (->> (sel root :input)
+       (filter partial-password-input?)))
+
+
+(defn find-editable-partial-password-inputs
+  "Scans the page for inputs that are characteristic to partial passwords"
+  [root]
+  (->> (find-partial-password-inputs root)
+       (remove #(dommy/attr % :disabled))
+       (remove #(dommy/attr % :readonly))))
+
 
 (defn input-id!
   "Returns the id of the given input, if input has no ID, then assigns a random one to it"
@@ -50,29 +113,49 @@
                                                                   :value value}})]
     (.dispatchEvent input event)))
 
-(defn on-input-change
-  "This function is called whenever the password input changes with the new password value"
-  [password input-map]
-  (debug (str "Password changed to " \" password \"))
-  (doseq [[idx input] input-map]
-    (simulate-user-input input (get password idx ""))))
-
 
 (defonce app-atom (r/atom {:password ""
                            :last-keypress-time (js/Date. 0)
                            :mode :main-menu
+                           :letter-inputs []
                            :global-input-map {}
                            :selected-letters (sorted-set)}))
+
+
+(defn get-input-map
+  "Returns an input map, either the global one or one based on picked characters"
+  [{:keys [mode global-input-map selected-letters letter-inputs]}]
+  (if (= mode :pick-chars)
+    (zipmap letter-inputs (concat selected-letters (repeat nil)))
+    global-input-map))
+
+
+(defn on-input-change'
+  [password input-map]
+  (debug (str "Password changed to " \" password \"))
+  (doseq [[input idx] input-map]
+    (simulate-user-input input (get password idx ""))))
+
+
+(defn on-input-change
+  "This function is called whenever the password input changes with the new password value"
+  []
+  (on-input-change' (:password @app-atom) (get-input-map @app-atom)))
+
 
 (defn update-password
   "Updates password in the app state atom and triggers callback"
   [new-password]
   (swap! app-atom assoc :password new-password)
-  (on-input-change (:password @app-atom) (:global-input-map @app-atom)))
+  (on-input-change))
 
 
 (defn set-global-input-map [input-map]
   (swap! app-atom assoc :global-input-map input-map))
+
+
+(defn set-letter-inputs [inputs]
+  (swap! app-atom assoc :letter-inputs inputs))
 
 
 (defn update-keypress-time
@@ -97,6 +180,7 @@
   "Opens pick characters menu"
   [evt]
   (swap! app-atom assoc :mode :pick-chars)
+  (on-input-change)
   (.stopPropagation evt))
 
 
@@ -104,6 +188,7 @@
   "Opens main menu"
   [evt]
   (swap! app-atom assoc :mode :main-menu)
+  (on-input-change)
   (.stopPropagation evt))
 
 
@@ -120,6 +205,13 @@
   [idx]
   (let [selected-letters (:selected-letters @app-atom)]
     (swap! app-atom assoc :selected-letters (toggle selected-letters idx))))
+
+
+(defn on-character-picked
+  "Toggles the character selection and updates the password"
+  [idx]
+  (toggle-selected-letter idx)
+  (on-input-change))
 
 
 (defn main-input-area
@@ -151,30 +243,30 @@
 
 (defn pick-char-button-class
   "Selects a class baseed on whether the input is selected or not"
-  [idx]
-  (if (contains? (:selected-letters @app-atom) idx)
+  [idx selected-letters]
+  (if (contains? selected-letters idx)
     :uk-button-primary
     :uk-button-default))
 
 
 (defn pick-char-button
   "Button to pick a given character password"
-  [idx]
+  [idx selected-letters]
   [:button.uk-button.uk-button-small.uk-button-default.uk-margin-small-right.uk-margin-bottom.password-helper-pick-chars-button
    {:key idx
-    :class (pick-char-button-class idx)
-    :on-click #(toggle-selected-letter idx)}
+    :class (pick-char-button-class idx selected-letters)
+    :on-click #(on-character-picked idx)}
    (str (inc idx))])
 
 
 (defn pick-chars-buttons
   "Renders pick chars buttons or a message if password is empty"
-  [password]
+  [password selected-letters]
 
   (if (empty? password)
     [:div.uk-text-warning "You need to type something into the password input first."]
     [:div.password-helper-grid.wrapping
-     (map pick-char-button (range (count password)))]))
+     (map #(pick-char-button % selected-letters) (range (count password)))]))
 
 
 (defn pick-chars
@@ -182,7 +274,7 @@
   []
   [:div.password-helper-pick-chars-container
    [:div.uk-card-title.uk-h4 "Pick Password Characters"]
-   (pick-chars-buttons (:password @app-atom))
+   [pick-chars-buttons (:password @app-atom) (:selected-letters @app-atom)]
    [:a.uk-link-muted.password-helper-pick-chars-close {:href "#" :on-click open-main-menu} "[X]"]])
 
 
@@ -194,6 +286,7 @@
    (case (:mode @app-atom)
      :main-menu [main-menu]
      :pick-chars [pick-chars])])
+
 
 (defn password-helper-app-root
   "This is the react root component for the password helper"
@@ -214,34 +307,43 @@
 
    [:div#password-helper-app-root]])
 
+
 (defn inject-password-helper-box
   "Injects password helper HTML element into the current page"
   [document input-map]
+
   (.appendChild (.-body document)
                 (hipo/create (password-helper-box)))
+
   (let [app-root (sel1 document :#password-helper-app-root)]
     (set-global-input-map input-map)
+    (set-letter-inputs (find-editable-partial-password-inputs document))
     (r/render [password-helper-app-root] app-root)))
+
 
 (defn password-helper-stylesheet
   "Link to the extension stylesheet"
   []
   [:link {:type "text/css" :rel "stylesheet" :href (get-css-asset-url "password_helper.css")}])
 
+
 (defn uikit-stylesheet
   "Link to the uikit stylesheet"
   []
   [:link {:type "text/css" :rel "stylesheet" :href (get-css-asset-url "uikit.min.css")}])
 
+
 (defn append-stylesheet [document html]
   "Append stylesheet tag to head element"
   (.appendChild (.-head document) (hipo/create html)))
+
 
 (defn inject-stylesheets
   "Injects password helper CSS into document HEAD"
   [document]
   (append-stylesheet document (password-helper-stylesheet))
   (append-stylesheet document (uikit-stylesheet)))
+
 
 (defn inject-html
   "Injects password helper HTML and stylesheet into the page"
@@ -250,14 +352,6 @@
   (inject-stylesheets document)
   (inject-password-helper-box document input-map))
 
-(defn ignore-errors
-  "Executes the given function and ignores errors thrown (logs them to console)"
-  [f]
-  (try
-    (f)
-    (catch :default e
-      (console/warn e)
-      nil)))
 
 (defn try-find
   "Tries to select the element, hides exceptions"
@@ -265,11 +359,13 @@
   (ignore-errors
     #(sel1 element selector)))
 
+
 (defn label-from-aria-label
   "Returns the label element based on the aria-labelledby attribute of the input"
   [input document]
   (if-let [label-id (dommy/attr input :aria-labelledby)]
     (try-find (.-body document) (str "#" label-id))))
+
 
 (defn label-from-input-id
   "If input has an ID try to look up label for it based on the for attribute"
@@ -277,10 +373,12 @@
   (if-let [id (dommy/attr input :id)]
     (try-find (.-body document) (str "label[for=" id "]"))))
 
+
 (defn label-nearby
   "Finds label that is in the same parent element as the input"
   [input]
   (sel1 (dommy/parent input) :label))
+
 
 (defn get-text
   "Returns element's text, handles nils gracefully"
@@ -288,23 +386,12 @@
   (if elem
     (dommy/text elem)))
 
+
 (defn find-parent
   "Recursively traverses the DOM to find a parent matching the given tag name"
   [element tag-name]
   (gdom/getAncestorByTagNameAndClass element tag-name))
 
-(defn partial-password-input?
-  "Checks if element looks like a partial password input"
-  [element]
-  (and
-    (= "password" (dommy/attr element :type))
-    (= "1" (dommy/attr element :maxlength))))
-
-(defn find-partial-password-inputs
-  "Scans the page for inputs that are characteristic to partial passwords"
-  [root]
-  (->> (sel root :input)
-       (filter partial-password-input?)))
 
 (defn index-in-parent
   "Returns a 0 based index of element in its parent node (includes only nodes of the same type)"
@@ -314,11 +401,13 @@
         children-of-same-type (filter #(= (.-tagName element) (.-tagName %)) children)]
     (.indexOf children-of-same-type element)))
 
+
 (defn position-in-table-row
   "If input is inside a table row, returns a 1-based index of its position in that row, nil otherwise"
   [input]
   (when-let [parent-cell (find-parent input "td")]
     (str (inc (index-in-parent parent-cell)))))
+
 
 (defn deepest-individual-ancestor
   "Returns the most nested ancestor that does not contain multiple partial password inputs"
@@ -329,6 +418,7 @@
         element
         (recur parent)))))
 
+
 (defn first-number
   "Returns first number found in text or nil"
   [text]
@@ -337,10 +427,12 @@
     (re-find #"(?i)last" (str text)) "0"
     :else (first (re-seq #"\d+" (str text)))))
 
+
 (defn get-text-if-number
   "Returns element text but only if the text contains a number, otherwise returns nil"
   [element]
   (first-number (get-text element)))
+
 
 (defn label-text-for-input
   "Return the label text for input based on the for attribute"
@@ -352,6 +444,7 @@
     (get-text-if-number (deepest-individual-ancestor input))
     (position-in-table-row input)))
 
+
 (defn index-for-input
   "Return the character index for editable password inputs"
   [input document]
@@ -359,14 +452,14 @@
         number (js/parseInt digits)]
     (dec number)))
 
+
 (defn build-index-input-map
   "Return a map of index => password field with password character indexes as keys"
   [document]
-  (->> (find-partial-password-inputs document)
-       (remove #(dommy/attr % :disabled))
-       (remove #(dommy/attr % :readonly))
-       (map #(vector (index-for-input % document) %))
+  (->> (find-editable-partial-password-inputs document)
+       (map #(vector % (index-for-input % document)))
        (into {})))
+
 
 (defn at-index
   "Returns character at index or empty string"
@@ -382,32 +475,12 @@
   [document]
   (sel1 document :#password-helper-box))
 
-(defn frame-documents
-  "Returns document objects for all frames found in document"
-  [document]
-  (letfn [(content-documents [tag]
-            (->> (->Array (sel document tag))
-                 (map (fn [frame] (ignore-errors #(.-contentDocument frame))))
-                 (remove nil?)))]
-    (concat
-      (content-documents :frame)
-      (content-documents :iframe))))
-
-(defn all-frame-docs
-  "Returns a collection of all frame documents on the page (recursively) including the top-level document"
-  ([]
-   (all-frame-docs [js/document]))
-
-  ([documents]
-   (if (seq documents)
-     (let [first-doc (first documents)]
-       (lazy-seq (cons first-doc (all-frame-docs (concat (rest documents) (frame-documents first-doc))))))
-     (lazy-seq))))
 
 (defn page-contains-partial-password?
   "Checks input on a page and returns true if the pages seems to contain a partial password"
   []
   (first (filter #(seq (find-partial-password-inputs %)) (all-frame-docs))))
+
 
 (defn start-password-helper
   "Start Password Helper on the page (add HTML, register event handlers, etc)"
@@ -416,12 +489,14 @@
     (debug "Injecting Password Helper")
     (inject-html document (build-index-input-map document))))
 
+
 (defn remove-password-helper
   "Removes password helper input from the page, if added"
   []
   (doseq [document (all-frame-docs)]
     (if-let [elem (find-password-helper-root document)]
       (dommy/remove! elem))))
+
 
 (defn listen-for-page-changes
   "If any change in DOM happens, checks if it seems like the page contains partial password and adds password helper in such case"
@@ -432,11 +507,13 @@
                                                     (remove-password-helper))))]
     (.observe mutation-observer js/document.body #js {:childList true :subtree true})))
 
+
 (defn wait-to-add-password-helper
   "Starts observing the page and adds Password Helper if necessary"
   []
   (debug "Partial password not found on page. Waiting to inject Password Helper until partial password shows up.")
   (listen-for-page-changes))
+
 
 (defn init-password-helper
   "Initialises password helper on page"
@@ -444,6 +521,7 @@
   (if-let [document (page-contains-partial-password?)]
     (start-password-helper document)
     (wait-to-add-password-helper)))
+
 
 (defn init
   "Main entry point of the application. Called from content.js"
